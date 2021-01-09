8a53743ce0235754f56681b219d364a1a1ed87e5
evfrancis <>
2021-01-09 00:44:28 -0500

    First commit

---
+++ fan.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import os
+import time
+import datetime
+
+pin = 4
+threshold = 70 
+filePath = "/home/pi/logs/fanLog.txt"
+
+file = open(filePath, mode="a", buffering=1)
+sleepInterval = 60
+fanEnabled = False
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+GPIO.setwarnings(True)
+
+def getTemperature():
+    res = os.popen('/opt/vc/bin/vcgencmd measure_temp').readline()
+    return float((res.replace("temp=","").replace("'C\n","")))
+
+def toggleFan(status):
+    global fanEnabled
+    GPIO.output(4, status)
+    fanEnabled = status
+
+def log(input):
+    date = str(datetime.datetime.utcnow())
+    file.write(str(date) + " " + input + "\n")
+
+try:
+    toggleFan(False)
+    while True:
+        log("Checking...")
+        temp = getTemperature()
+        if temp > threshold:
+            if not fanEnabled:
+                log("Turning on fan, temperature=" + str(temp))
+                toggleFan(True)
+            else:
+                log("Temperature=" + str(temp))
+        elif fanEnabled:
+            log("Turning off fan, temperature=" + str(temp))
+            toggleFan(False)
+        time.sleep(sleepInterval)
+except:
+    log("Closing")
+    GPIO.cleanup()
---
+++ plants.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from picamera import PiCamera
+from time import sleep
+import os
+import datetime
+
+outputDirBase = "/home/pi/camera"
+
+def takePicture(output):
+    camera.start_preview()
+    sleep(5)
+    camera.capture(output)
+    camera.stop_preview()
+    sleep(1)
+
+def buildOutputPath(date):
+    path = "{}/{}/{}/{}".format(outputDirBase, date.year, date.month, date.day)
+    os.makedirs(path)
+    path = "{}/image_{}.jpg".format(path, date.hour)
+    return path
+
+date = datetime.datetime.utcnow()
+path = buildOutputPath(date)
+camera = PiCamera()
+takePicture(path)
